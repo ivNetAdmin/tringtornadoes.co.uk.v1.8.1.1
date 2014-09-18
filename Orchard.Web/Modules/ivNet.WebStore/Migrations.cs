e138cf1ed43402b17395a5e9f152390988d5c94a
bp <bp@ivnet.co.uk>
2014-09-18 09:19:24 +0100

    working cart

--- Orchard.Web/Modules/ivNet.WebStore/Migrations.cs
+++ Orchard.Web/Modules/ivNet.WebStore/Migrations.cs
@@ -12,9 +12,11 @@ using ivNet.Webstore.Models;
 using Orchard.Widgets.Models;
 
 namespace ivNet.Webstore {
-    public class Migrations : DataMigrationImpl {
-        
-        public int Create() {
+    public class Migrations : DataMigrationImpl
+    {
+
+        public int Create()
+        {
             SchemaBuilder.CreateTable("ProductRecord", table => table
                 .ContentPartRecord()
                 .Column<decimal>("Price")
@@ -24,8 +26,9 @@ namespace ivNet.Webstore {
             return 1;
         }
 
-        public int UpdateFrom1() {
-            ContentDefinitionManager.AlterPartDefinition(typeof(ProductPart).Name, part => part
+        public int UpdateFrom1()
+        {
+            ContentDefinitionManager.AlterPartDefinition(typeof (ProductPart).Name, part => part
                 .Attachable()
                 );
 
@@ -35,9 +38,9 @@ namespace ivNet.Webstore {
         public int UpdateFrom2()
         {
             ContentDefinitionManager.AlterTypeDefinition("ShoppingCartWidget", type => type
-                .WithPart(typeof(ShoppingCartWidgetPart).Name)
-                .WithPart(typeof(CommonPart).Name)
-                .WithPart(typeof(WidgetPart).Name)
+                .WithPart(typeof (ShoppingCartWidgetPart).Name)
+                .WithPart(typeof (CommonPart).Name)
+                .WithPart(typeof (WidgetPart).Name)
                 .WithSetting("Stereotype", "Widget")
                 );
 
@@ -47,13 +50,14 @@ namespace ivNet.Webstore {
         public int UpdateFrom3()
         {
             ContentDefinitionManager.AlterTypeDefinition("ShoppingCartWidget", type => type
-                .WithPart(typeof(CommonPart).Name)
+                .WithPart(typeof (CommonPart).Name)
                 );
 
             return 4;
         }
 
-        public int UpdateFrom4() {
+        public int UpdateFrom4()
+        {
             SchemaBuilder.CreateTable("CustomerRecord", t => t
                 .ContentPartRecord()
                 .Column<string>("FirstName", c => c.WithLength(50))
@@ -68,36 +72,38 @@ namespace ivNet.Webstore {
                 .Column<string>("Type", c => c.WithLength(50))
                 );
 
-            SchemaBuilder.CreateForeignKey("Address_Customer", "AddressRecord", new[] {"CustomerId"}, "CustomerRecord", new[] {"Id"});
+            SchemaBuilder.CreateForeignKey("Address_Customer", "AddressRecord", new[] {"CustomerId"}, "CustomerRecord",
+                new[] {"Id"});
 
-            ContentDefinitionManager.AlterPartDefinition(typeof(CustomerPart).Name, p => p
+            ContentDefinitionManager.AlterPartDefinition(typeof (CustomerPart).Name, p => p
                 .Attachable(false)
-                .WithField("Phone", f => f.OfType(typeof(TextField).Name))
+                .WithField("Phone", f => f.OfType(typeof (TextField).Name))
                 );
 
             ContentDefinitionManager.AlterTypeDefinition("Customer", t => t
-                .WithPart(typeof(CustomerPart).Name)
-                .WithPart(typeof(UserPart).Name)
+                .WithPart(typeof (CustomerPart).Name)
+                .WithPart(typeof (UserPart).Name)
                 );
 
-            ContentDefinitionManager.AlterPartDefinition(typeof(AddressPart).Name, p => p
+            ContentDefinitionManager.AlterPartDefinition(typeof (AddressPart).Name, p => p
                 .Attachable(false)
-                .WithField("Name", f => f.OfType(typeof(TextField).Name))
-                .WithField("AddressLine1", f => f.OfType(typeof(TextField).Name))
-                .WithField("AddressLine2", f => f.OfType(typeof(TextField).Name))
-                .WithField("Zipcode", f => f.OfType(typeof(TextField).Name))
-                .WithField("City", f => f.OfType(typeof(TextField).Name))
-                .WithField("Country", f => f.OfType(typeof(TextField).Name))
+                .WithField("Name", f => f.OfType(typeof (TextField).Name))
+                .WithField("AddressLine1", f => f.OfType(typeof (TextField).Name))
+                .WithField("AddressLine2", f => f.OfType(typeof (TextField).Name))
+                .WithField("Zipcode", f => f.OfType(typeof (TextField).Name))
+                .WithField("City", f => f.OfType(typeof (TextField).Name))
+                .WithField("Country", f => f.OfType(typeof (TextField).Name))
                 );
 
             ContentDefinitionManager.AlterTypeDefinition("Address", t => t
-                .WithPart(typeof(AddressPart).Name)
+                .WithPart(typeof (AddressPart).Name)
                 );
 
             return 5;
         }
 
-        public int UpdateFrom5() {
+        public int UpdateFrom5()
+        {
             SchemaBuilder.CreateTable("OrderRecord", t => t
                 .Column<int>("Id", c => c.PrimaryKey().Identity())
                 .Column<int>("CustomerId", c => c.NotNull())
@@ -105,6 +111,7 @@ namespace ivNet.Webstore {
                 .Column<decimal>("Total", c => c.NotNull())
                 .Column<decimal>("SubTotal", c => c.NotNull())
                 .Column<decimal>("Vat", c => c.NotNull())
+                .Column<string>("Number", c => c.WithLength(255))
                 .Column<string>("Status", c => c.WithLength(50).NotNull())
                 .Column<string>("PaymentServiceProviderResponse", c => c.WithLength(null))
                 .Column<string>("PaymentReference", c => c.WithLength(50))
@@ -120,13 +127,20 @@ namespace ivNet.Webstore {
                 .Column<int>("Quantity", c => c.NotNull())
                 .Column<decimal>("UnitPrice", c => c.NotNull())
                 .Column<decimal>("VatRate", c => c.NotNull())
+                .Column<decimal>("Vat", c => c.NotNull())
+                .Column<decimal>("UnitVat", c => c.NotNull())
+                 .Column<decimal>("Total", c => c.NotNull())
+                .Column<decimal>("SubTotal", c => c.NotNull())
                 );
 
-            SchemaBuilder.CreateForeignKey("Order_Customer", "OrderRecord", new[] {"CustomerId"}, "CustomerRecord", new[] {"Id"});
-            SchemaBuilder.CreateForeignKey("OrderDetail_Order", "OrderDetailRecord", new[] { "OrderRecord_Id" }, "OrderRecord", new[] { "Id" });
-            SchemaBuilder.CreateForeignKey("OrderDetail_Product", "OrderDetailRecord", new[] { "ProductId" }, "ProductRecord", new[] { "Id" });
+            SchemaBuilder.CreateForeignKey("Order_Customer", "OrderRecord", new[] {"CustomerId"}, "CustomerRecord",
+                new[] {"Id"});
+            SchemaBuilder.CreateForeignKey("OrderDetail_Order", "OrderDetailRecord", new[] {"OrderRecord_Id"},
+                "OrderRecord", new[] {"Id"});
+            SchemaBuilder.CreateForeignKey("OrderDetail_Product", "OrderDetailRecord", new[] {"ProductId"},
+                "ProductRecord", new[] {"Id"});
 
             return 6;
-        }       
+        }      
     }
 }
